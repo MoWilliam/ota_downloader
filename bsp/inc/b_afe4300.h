b00012a62186f04e59cc9f57034f4362e31985eb
wangwei <wangwei@bitnei.com>
2023-07-19 15:28:26 +0800

    卡尔曼滤波函数简化，并设参数。

--- bsp/inc/b_afe4300.h
+++ bsp/inc/b_afe4300.h
@@ -12,20 +12,7 @@
 
 #include "application.h"
 
-//1维的卡尔曼滤波
-typedef struct {
-    float x;  // state 
-    float A;  // x(n)=A*x(n-1)+u(n),u(n)~N(0,q) 
-    float H;  // z(n)=H*x(n)+w(n),w(n)~N(0,r)   
-    float q;  // process(predict) noise convariance 协方差
-    float r;  // measure noise convariance 
-    float p;  // estimated error convariance 估计误差协方差
-    float gain;
-}kalman_struct;
-
-void kalman_init(kalman_struct *kalman_lcw, float init_x, float init_p);
-float kalman_filter(kalman_struct *kalman_lcw, float z_measure);
-
+float kalman_filter(float inData);
 void bsp_afe4300_init(void);
 void bsp_afe4300_get(BioFrameDef* dmf);
 
